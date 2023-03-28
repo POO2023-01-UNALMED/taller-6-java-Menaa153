0be77711b99d03f4ec89e48b164346cdede79314
Menaa153 <cimbacuan@unal.edu.co>
2023-03-28 01:31:48 -0500

    test

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -4,13 +4,13 @@ public class Camioneta extends Vehiculo {
     private boolean volco;
     private static int cantidadCamionetas;
 
-    public Camioneta(String placa, int puertas, String nombre, int precio, int peso, boolean volco, Fabricante fabricante) {
+    public Camioneta(String placa, int puertas, String nombre, int precio, int peso,  Fabricante fabricante, boolean volco) {
         super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
         this.volco = volco;
         cantidadCamionetas++;
     }
 
-    public boolean tieneVolco() {
+    public boolean isVolco() {
         return volco;
     }
 
