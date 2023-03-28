7df731401b899346090eccaf701ad45ca0b2d0fe
Menaa153 <cimbacuan@unal.edu.co>
2023-03-27 23:35:45 -0500

    test

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,8 +1,12 @@
-
 package vehiculos;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Pais {
     private String nombre;
+    private int unidadesVendidas;
+    private static Map<String, Integer> ventasPorPais=new HashMap<>(); // mapa de ventas por pais
 
     public Pais(String nombre) {
         this.nombre = nombre;
@@ -15,5 +19,26 @@ public class Pais {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public int getUnidadesVendidas(){
+        return unidadesVendidas;
+    }
+
+    public static void registrarVenta(String país) {
+        ventasPorPais.put(país, ventasPorPais.getOrDefault(país, 0) + 1); // registrar venta para el país
+    }
+
+    public static String paisMasVendedor() {
+        String paísMasVendedor = null;
+        int unidadesVendidas = 0;
+        for (Map.Entry<String, Integer> venta : ventasPorPais.entrySet()) {
+            if (venta.getValue() > unidadesVendidas) {
+                unidadesVendidas = venta.getValue();
+                paísMasVendedor = venta.getKey();
+            }
+        }
+        return paísMasVendedor;
+    }
 }
+
 
